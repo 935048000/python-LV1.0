7cccb7622bfc18832550e2282c254a7bf0e20950
vast <935048000@qq.com>
2017-12-28 10:27:45 +0800

    数据可视化

---
+++ pygal_test/__init__.py
@@ -0,0 +1,5 @@
+# a = [[1,1,1],[2,2,2]]
+
+# print(len(a))
+# for i in a :
+#     print(i)
--- pygal_test/data2svg.py
+++ pygal_test/data2svg.py
@@ -3,7 +3,7 @@ from sys import argv
 from os import path
 import pygal
 from datetime import datetime, timedelta
-
+from operator import itemgetter
 
 def getdata(FILE):
     """
@@ -33,45 +33,48 @@ def data2list(data):
     :param data:
         data --> list
     :return: tickte1,tickte2,x_date,link_name
-        tickte1:官微的票数
-        tickte2：官博的票数
+        tickte1:票数
         x_date：X轴的时间点
         link_name：折线的线名
     """
-    # 官微
-    data1 = []
-    for x in data:
-        if x[1] == 74:
-            data1.append(x)
-
-
-    tickte1 = []
-    x_date = []
-    for x1 in data1:
-        tickte1.append (x1[0])
-        x_date.append (x1[3][11:])
-    # print(data1)
-
-    # 官博
-    data2 = []
-    for y in data:
-        if y[1] == 73:
-            data2.append(y)
-
-    tickte2 = []
-    for y1 in data2:
-        tickte2.append(y1[0])
-
     # 线名
     link_name = []
-    link_name.append (data1[1][2])
-    link_name.append (data2[1][2])
+    # 票数
+    tickte = []
+    # 时间
+    x_date = []
+    # 票名ID
+    voteName = []
 
-    return tickte1,tickte2,x_date,link_name
+    voteID = data[-134:]
+    voteID.sort(key=itemgetter(0),reverse=True)
+    for k in voteID[:10]:
+        voteName.append(k[1])
+    voteName.append(73)
+
+
+    for i in voteName:
+        data2 = []
+        for y in data:
+            if y[1] == i:
+                data2.append (y)
+
+        link_name.append (data2[1][2])
+
+        tickte2 = []
+        for x in data2:
+            tickte2.append (x[0])
+
+            if len(x_date) <= 134:
+                x_date.append (x[3][11:])
+
+        tickte.append(tickte2)
+
+    return tickte,x_date,link_name
 
 
 
-def getSVG(tickte1,tickte2,x_date,link_name,Ddate,svgname):
+def getSVG(tickte,x_date,link_name,Ddate,svgname):
     """
     :param tickte1: 官微的票数
     :param tickte2: 官博的票数
@@ -89,8 +92,11 @@ def getSVG(tickte1,tickte2,x_date,link_name,Ddate,svgname):
     title_name =  Ddate + '日的票数增长图'
     line_chart.title = title_name
     line_chart.x_labels = x_date
-    line_chart.add(link_name[0],  tickte1)
-    line_chart.add(link_name[1],  tickte2)
+    for i,j in zip(tickte,link_name):
+        line_chart.add(j,  i)
+
+
+    # line_chart.add(link_name[1],  tickte2)
     line_chart.render_to_file(svgname)
 
     return 0
@@ -106,10 +112,12 @@ if __name__ == '__main__':
     except:
         print ("[ERROR] Please input command")
 
-    # FILE = "./data_2017-12-21.txt"
+    # FILE = "./data_2017-12-22.txt"
     data = getdata(FILE)
-    tickte1, tickte2, x_date, link_name = data2list(data)
+    tickte,x_date, link_name = data2list(data)
     Ddate = FILE[:-4]
     Ddate = Ddate[-10:]
     SVGname = "%s.svg"% Ddate
-    getSVG (tickte1, tickte2, x_date, link_name, Ddate, SVGname)
+    getSVG (tickte,x_date, link_name, Ddate, SVGname)
+
+
