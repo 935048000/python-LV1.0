a13085e60fbf6b6d47f8e6b27d1f77a0f58a8b91
chenhao <935048000@qq.com>
2018-10-18 15:31:43 +0800

    web爬虫

---
+++ Crawler/getCSDNReadNumber.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime
+from re import sub
+from time import sleep
+
+html = urlopen ("https://me.csdn.net/ChenVast")
+bsObj = BeautifulSoup (html, "html.parser")
+data = bsObj.find ('body').find ('div', {'class': 'tab_page my_tab_page'}).find ('ul', {
+    'class': 'mod_my_t clearfix'}).get_text ()
+print (data.strip ("\n").split ("\n")[3])
