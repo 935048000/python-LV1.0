556f67c6a7459730e4784c44dfde963ea138f5a2
chenhao <935048000@qq.com>
2018-10-24 11:16:11 +0800

    debug

---
+++ debug/debugging.py
@@ -0,0 +1,11 @@
+def hello():
+    print ("input hell()")
+    return 'hello'
+
+
+if __name__ == '__main__':
+    print ("begin")
+    print ('###' * 10)
+    # name = input("Please input your name: ")
+    print (hello ())
+    print ('###' * 10)
