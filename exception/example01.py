95a1fa29079e792ef1c7007ac8df0f5ec5922042
chenhao <935048000@qq.com>
2018-10-29 10:58:21 +0800

    异常处理

--- exception/example01.py
+++ exception/example01.py
@@ -4,14 +4,20 @@ def div(a, b):
     except ZeroDivisionError:
         print ('Error:b 不能等于0')
     except Exception as e:
+        # 对异常增加解释
         e.args += ('more test...........',)
+    
+        # 不中断执行，打印/输出 异常
         print ('意想不到的错误', e)
-        
+    
+        # 中断执行，返回异常
         # raise
     else:
         print ('一切顺利')
     finally:
         print ('总是执行')
+
+    print ('run right!')
     return 0
 
 
